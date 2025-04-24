3d4c16c6fbb27e439b5e7d9818c528e853c4844c
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2025-04-24 22:25:47 +0300

    wrong

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
 
+import java.util.Scanner;
+
 public class Main {
+    Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Hello World!");
         for (int i = 0; i < 10; i++) {
