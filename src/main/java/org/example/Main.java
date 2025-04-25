816b8c5137ffa05959d13d796da41583985b79e4
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2025-04-25 11:56:41 +0300

    add str number

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 
 public class Main {
     Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
         for (int i = 0; i < 10; i++) {
-            System.out.println(i + 100);
+            System.out.println("Number" + i + 100);
         }
     }
 }
