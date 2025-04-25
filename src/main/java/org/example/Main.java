0f9a6426b46ff07821a8c4068f37cdc1e8b79a96
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2025-04-25 12:08:11 +0300

    add int10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
     Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        int a = 10;
         System.out.println("Hello World!");
         for (int i = 0; i < 10; i++) {
             System.out.println("Number" + i + 100);
